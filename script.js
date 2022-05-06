98d0b48e4f2a3d31d454bcd28e2425d0e866b3e1
Zack Brooks <zibrooks@hotmail.com>
2022-05-06 00:59:52 -0500

    added a text to speak that says the winner of the game. More refactoring as well as some CSS. Added overlay upon game completion as well

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ let board = document.getElementById('bored')
 let playerOne = document.getElementById('playerOne').classList
 let playerTwo = document.getElementById('playerTwo').classList
 let resetBtn = document.getElementById('reset')
+let overLayText = document.getElementById('text')
 let game = ['', '', '', '', '', '','', '', '']
+let clickCount = 0
+
 
 let playerObj = {
     activePlayerToggle() { // this changes the active player
@@ -14,12 +17,14 @@ let playerObj = {
         addClass.classList.add('used')
         document.getElementById(id).textContent = 'X'
         game[id] = 'X'
+        clickCount++
     },
     playerOMark(addClass, id) { // Behavior for when Player O is active player
         addClass.classList.add('clickO')
         addClass.classList.add('used')
         document.getElementById(id).textContent = 'O'
         game[id] = 'O'
+        clickCount++
     }
 }
 
@@ -50,7 +55,6 @@ let gameObj = {
     gameState() {
         //checks if there is a winner after each player mark
         let roundWon = false;
-        let stopTie = 0
        for(let i = 0; i <= 7; i++){
          const winCondition = this.winningConditions[i];
          let a = game[winCondition[0]];
@@ -59,26 +63,34 @@ let gameObj = {
          if (a === '' || b === '' || c === '') {
                  continue;
          }
-          if(a === b && b === c) {
+          if(a === b && b === c) { // player wins
                  roundWon = true;
                  playerObj.activePlayerToggle() //OCD just keeping the winner as the active player
                  let winArr = this.winningConditions[i]
-                 winArr.forEach((x,z) => document.getElementById(winArr[z]).style.background = "gold" ) // changes background of winning cells to red
-                 console.log(`Player ${a} wins!`)
-                 board.removeEventListener('click', gameObj.gameBehavior)
-                 stopTie++
-                 break
+                 winArr.forEach((x,z) => document.getElementById(winArr[z]).style.background = "red" ) // changes background of winning cells to red
+                 overLayText.textContent = `Player ${a} wins! Hit reset to play again!` // displays winner msg on an overlay
+                 board.removeEventListener('click', gameObj.gameBehavior) // removes the board eventlister so user can't click anymore
+                 this.on() // makes overlay visible
+                 window.speechSynthesis.speak(new SpeechSynthesisUtterance(`Winner winner chicken dinner. Player ${a} wins! Hit reset to play again!`))
+                 break         
          } 
-        }  roundWon && stopTie < 1 ? console.log('Game Tied') : null // tells user game is tied
+        } 
+        if(roundWon === false && clickCount === 9) { //behavior for when there is a tie
+            overLayText.textContent = `Game is tied! Hit reset to play again!`
+            this.on()
+            window.speechSynthesis.speak(new SpeechSynthesisUtterance(`Game is tied! Hit reset to play again!`))
+        }
     },   
     //resets the gamestate
     resetGame() {
         for(let i = 0; i < 9; i++) {
+            document.getElementById("overlay").style.display = "none";
             document.getElementById(i).style.background = "white" 
             document.getElementById(i).textContent = "" // removes marks from boxes
             document.getElementById(i).classList.remove('used') //removes used class so you can mark box again
             game[i] = "" // resets array used for CheckWin
-            board.addEventListener('click', gameObj.gameBehavior)         
+            board.addEventListener('click', gameObj.gameBehavior) 
+                 
         }
         playerOne.contains('activePlayer') ? null : playerObj.activePlayerToggle() // makes PlayerX the active player
     },
@@ -87,7 +99,11 @@ let gameObj = {
             let addClass = document.getElementById(`${e.target.id}`) // grabs id of the clicked box
             gameObj.placeMark(addClass, e.target.id) // uses clicked box id to run function allowing player to mark
             }    
-    }
+    },
+    on() { // turns winning overlay on
+        document.getElementById("overlay").style.display = "block";
+      }
+      
 }
 
 
