96d7ce8d29b4f3287b67869afb5f5f6e0675e6ac
Zack Brooks <zibrooks@hotmail.com>
2022-05-05 21:05:45 -0500

    put some functions in an obj

--- script.js
+++ script.js
@@ -5,82 +5,12 @@ let resetBtn = document.getElementById('reset')
 playerTwo.classList.remove('activePlayer')
 playerOne.classList.add('activePlayer')
 let turnCounter = 0
-
 let game = ['', '', '', '', '', '','', '', '']
-const winningConditions = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-];
-// function used to check if there is a winner or if game is tied
-function gameState() {
-   let roundWon = false;
-  for(let i = 0; i <= 7; i++){
-    const winCondition = winningConditions[i];
-    let a = game[winCondition[0]];
-    let b = game[winCondition[1]];
-    let c = game[winCondition[2]];
-    if (a === '' || b === '' || c === '') {
-            continue;
-    }
-     if (a === b && b === c) {
-            roundWon = true;
-            console.log(`Player ${a} wins!`)
-            break
-    }
-    
-    if(turnCounter == 9){
-      console.log('Game Tied!')
-    }
-}}
 
-// Reset Game
-function resetGame() {
-    for(let i = 0; i < 9; i++) {
-        document.getElementById(i).textContent = "" // removes marks from boxes
-        document.getElementById(i).classList.remove('used') //removes used class so you can mark box again
-        game[i] = "" // resets array used for CheckWin
-        turnCounter = 0 // resets turnCounters
-    }
-}
+let gameObj = {
 
-// function used to check if there is a winner or if game is tied
-
-// function checkWin() {
-//     if(game[0] === 'X' && game[1] === 'X' && game[2] === 'X' ||
-//        game[3] === 'X' && game[4] === 'X' && game[5] === 'X' ||
-//        game[6] === 'X' && game[7] === 'X' && game[8] === 'X' ||
-//        game[0] === 'X' && game[3] === 'X' && game[6] === 'X' ||
-//        game[1] === 'X' && game[4] === 'X' && game[7] === 'X' ||
-//        game[2] === 'X' && game[5] === 'X' && game[8] === 'X' ||
-//        game[0] === 'X' && game[4] === 'X' && game[8] === 'X' ||
-//        game[2] === 'X' && game[4] === 'X' && game[6] === 'X' 
-//     ) {
-//         alert('Player X wins! Click New Game to play again!')
-//         board.removeEventListener('click', e => {})
-//     } else if (game[0] === 'O' && game[1] === 'O' && game[2] === 'O' ||
-//     game[3] === 'O' && game[4] === 'O' && game[5] === 'O' ||
-//     game[6] === 'O' && game[7] === 'O' && game[8] === 'O' ||
-//     game[0] === 'O' && game[3] === 'O' && game[6] === 'O' ||
-//     game[1] === 'O' && game[4] === 'O' && game[7] === 'O' ||
-//     game[2] === 'O' && game[5] === 'O' && game[8] === 'O' ||
-//     game[0] === 'O' && game[4] === 'O' && game[8] === 'O' ||
-//     game[2] === 'O' && game[4] === 'O' && game[6] === 'O' ) {
-//         alert('Player O wins! Click New Game to play again!')
-//         board.removeEventListener('click', e => {})
-//     } else if(turnCounter == 9){
-//                 alert('Game Tied! Click New Game to play again!')
-//                 board.removeEventListener('click', e => {})
-//               }
-// }
-
-// puts mark from active player onto the gameboard
-function placeMark(addClass, id) {
+    // puts mark from active player onto the gameboard, after it mark runs gamestate and changes active player if game isnt over
+    placeMark(addClass, id) {
     turnCounter++
     if(playerOne.classList.contains('activePlayer')){
         if(addClass.classList.contains('used')){
@@ -92,7 +22,7 @@ function placeMark(addClass, id) {
             playerTwo.classList.add('activePlayer')
             document.getElementById(id).textContent = 'X'
             game[id] = 'X'
-            gameState()
+            this.gameState()
         }
     } else if(playerTwo.classList.contains('activePlayer')){
         if(addClass.classList.contains('used')){
@@ -104,17 +34,61 @@ function placeMark(addClass, id) {
             playerOne.classList.add('activePlayer')
             document.getElementById(id).textContent = 'O'
             game[id] = 'O'
-            gameState()
+            this.gameState()
         }
     }
-
+},
+    
+    gameState() {
+        let roundWon = false;
+        let stopTie = 0
+       for(let i = 0; i <= 7; i++){
+         const winCondition = winningConditions[i];
+         let a = game[winCondition[0]];
+         let b = game[winCondition[1]];
+         let c = game[winCondition[2]];
+         if (a === '' || b === '' || c === '') {
+                 continue;
+         }
+          if(a === b && b === c) {
+                 roundWon = true;
+                 console.log(`Player ${a} wins!`)
+                 stopTie++
+                 console.log(stopTie)
+                 break
+         } else if(turnCounter == 9 && stopTie === 0){
+           console.log('Game Tied!')
+         }
+     }},
+    
+    resetGame() {
+        for(let i = 0; i < 9; i++) {
+            document.getElementById(i).textContent = "" // removes marks from boxes
+            document.getElementById(i).classList.remove('used') //removes used class so you can mark box again
+            game[i] = "" // resets array used for CheckWin
+            turnCounter = 0 // resets turnCounters
+        }
+    }
 }
+
+
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
 
 board.addEventListener('click', e => {
     if(e.target.classList.contains('boxes')) { //checks to make sure clicked target is a box and not empty space
     let addClass = document.getElementById(`${e.target.id}`)
-    placeMark(addClass, e.target.id)
+    gameObj.placeMark(addClass, e.target.id)
     }
     
 })
-resetBtn.addEventListener("click", resetGame)
+resetBtn.addEventListener("click", gameObj.resetGame)
